1c1a49a83436ad6b606c9e98b92c23c10969ea4e
Joseph Kim <youngchk@andrew.cmu.edu>
2017-06-19 16:10:46 -0400

    Update gradient.js

--- js/gradient.js
+++ js/gradient.js
@@ -2,5 +2,5 @@
 function update_gradient(event){
   var theta = Math.atan(event.clientY/event.clientX)*180/Math.PI;
   var degrees = theta + 'deg';
-  document.querySelector('body').style.background = linear-gradient(degrees, cyan, blue);
+  document.querySelector('body').style.background = "linear-gradient("+degrees+", cyan, blue)";
 }
