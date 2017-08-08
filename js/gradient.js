654817276beb42a7fa21f04d5e313784e4c1c988
qwewy <youngchk@Josephs-MacBook-Pro.local>
2017-08-07 22:26:37 -0400

    new picture and icons

--- js/gradient.js
+++ js/gradient.js
@@ -1,6 +1,8 @@
 
 function update_gradient(event){
-  //var theta = Math.atan(event.clientY/event.clientX)*180/Math.PI;
-  var degrees = event.clientX - event.clientY + 'deg';
+  var theta = Math.atan(event.clientY/event.clientX)*180/Math.PI;
+  //var degrees = event.clientX - event.clientY + 'deg';
+  var degrees = theta + 'deg'
   document.querySelector('body').style.background = "linear-gradient("+degrees+", cyan, blue)";
-}
+
+}			
